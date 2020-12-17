10bd7b466af24409c53f3fde84537d48df5f7fff
arnaupool <arnyr@hotmail.es>
2020-12-18 00:19:46 +0100

    Añadida funcionalidad de la página de inicio: Según el botón que pulses se abre un componente u otro // Creados componentes de listar historiales y revisiones de una nave

---
+++ GestionMarcianitos/src/app/revision/listarrevision/listarrevision.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listarrevision',
+  templateUrl: './listarrevision.component.html',
+  styleUrls: ['./listarrevision.component.css']
+})
+export class ListarrevisionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
